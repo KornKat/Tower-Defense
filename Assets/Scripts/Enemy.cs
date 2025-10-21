c8c81a364deab5f2099c76f72ff164dfeac230ef
KornKat <kk918@student.aru.ac.uk>
2025-10-21 11:57:29 +0100

    Added enemies and wave spawn

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointindex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointindex++;
+        target = Waypoints.points[wavepointindex];
+    }
+}
