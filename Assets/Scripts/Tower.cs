26e01400f44ef90167463e0eec0469e4ab3b3511
KornKat <kk918@student.aru.ac.uk>
2025-10-28 09:02:10 +0000

    Added a countdown and a tower

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class WaveSpawner : MonoBehaviour
 
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    public Text waveCountdownText;
 
     private int waveIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -29,6 +32,8 @@ public class WaveSpawner : MonoBehaviour
         }
 
         countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
     }
 
     IEnumerator SpawnWave ()
