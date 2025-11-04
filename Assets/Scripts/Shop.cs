02b0ed01d68482ea2748cd43367e94078a006ba2
KornKat <kk918@student.aru.ac.uk>
2025-11-04 11:55:31 +0000

    added adjustable camera and shop ui

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public void PurchaseTower()
+    {
+        Debug.Log("Tower Purchased");
+    }
+
+    public void PurchaseAnotherTower()
+    {
+        Debug.Log("Another Tower Purchased");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
