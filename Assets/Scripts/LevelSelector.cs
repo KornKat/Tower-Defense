6a74a1be67faf873177d06dcd31593cd2711eb93
kornkat <kornelijus.katinas@gmail.com>
2025-12-07 00:20:01 +0000

    refresh

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -6,10 +6,17 @@ public class LevelSelector : MonoBehaviour
 {
     public string backToMenu = "MainMenu";
     public string levelOne = "LevelOne";
+    public string levelTwo = "LevelTwo";
 
     public void FirstLevel()
     {
         SceneManager.LoadScene(levelOne);
+        Time.timeScale = 1f;
+
+    }
+    public void SecondLevel()
+    {
+        SceneManager.LoadScene(levelTwo);
         Time.timeScale = 1f;
 
     }
